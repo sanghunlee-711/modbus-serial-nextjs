f16861d5854705432cf1e74accdaf4371970c2f4
cloudlee <cloudlee@MacBook-Pro-9.local>
2022-06-14 14:00:56 +0900

    [Add] Buffer Console

--- lib/serial.ts
+++ lib/serial.ts
@@ -33,8 +33,8 @@ export const serialServerOn = () => {
 export const serialOpen = () => {
   // let open = '01 06 00 15 00 01 59 ce'; //16
   let openArr = [1, 6, 0, 21, 0, 1, 89, 206];
-  console.log(openArr);
   let openBuff = Buffer.from(openArr);
+  console.log({ openBuff });
   // 0000 0001 / 0000 0110 / 0000 0000/ 0001 0101/ 0000 0000/ 0000 0001/ 0101 1001 / 1100 1110
   // 1                  /6           /0 /21/ 0/1/89/ 216
   serialServer.write(openBuff, (error) => {
@@ -43,9 +43,10 @@ export const serialOpen = () => {
 };
 
 export const serialClose = () => {
-  let close = '01 /06/ 00/ 15/ 00/ 00/ 98 0e';
+  // let close = '01 /06/ 00/ 15/ 00/ 00/ 98 0e';
   const closeArr = [1, 6, 0, 21, 0, 0, 152, 14];
   const closeBuff = Buffer.from(closeArr);
+  console.log({ closeBuff });
   serialServer.write(closeBuff, (error) => {
     console.error({ writeError: error });
   });
